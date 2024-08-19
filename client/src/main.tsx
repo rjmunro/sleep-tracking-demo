734cd05208d3a57b9009d05596b3d3228fdc8805
Robert (Jamie) Munro <rjmunro@arjam.net>
2024-08-19 17:00:41 +0100

    Format H1 with some tailwind classes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,7 +9,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <h1>Cynomi Tech Task</h1>
+      <h1 className="text-4xl font-bold text-gray-800">Cynomi Tech Task</h1>
     </QueryClientProvider>
   </React.StrictMode>
 );
