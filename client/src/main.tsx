fd1a9436e215fb821cff07216051f16ab0280a91
Robert (Jamie) Munro <rjmunro@arjam.net>
2024-08-19 14:10:29 +0100

    Add PeopleTableComponent
    And wrap everything in a component so it can have state hoisted.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import "./index.css";
 import FormComponent from "./FormComponent";
-
+import UsersTableComponent from "./UsersTableComponent";
 
 const queryClient = new QueryClient();
+
+function SleepTracker() {
+  const [selectedUserId, setSelectedUserId] = useState<number|null>(null);
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-gray-800">Cynomi Tech Task</h1>
+      <FormComponent />
+      <UsersTableComponent
+        selectedUserId={selectedUserId}
+        setSelectedUserId={setSelectedUserId}
+      />
+    </>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <h1 className="text-4xl font-bold text-gray-800">Cynomi Tech Task</h1>
-      <FormComponent />
+      <SleepTracker />
     </QueryClientProvider>
   </React.StrictMode>
 );
